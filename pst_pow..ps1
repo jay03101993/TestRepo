4d9ff18c8fd82f697fde16a55c8e74c41aa8d8fc
jay03101993 <jacharjee@huronconsultinggroup.com>
2021-02-18 22:52:44 +0530

    added negative scenario testing

--- pst_pow..ps1
+++ pst_pow..ps1
@@ -1,3 +1,7 @@
 echo on 
 cd C:\ApiReports\Collections\GitCodeApi
 echo "navigated to location"
+newman run Test.json -e TestEnv.json
+echo "running postman rest api test"
+echo "generating report"
+newman run Test.json -e TestEnv.json -r htmlextra
